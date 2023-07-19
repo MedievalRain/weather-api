c92f62c43b980fe8a58977ae15d6991c0d2de71f
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 01:10:12 +0300

    Enable CORS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+        ],
+      },
+    ];
+  },
+};
